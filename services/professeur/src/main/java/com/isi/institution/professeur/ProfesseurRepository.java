8a1bce75068875c53c859f80308efb539c3efb40
LinhoDevops <aliou.18.ndour@gmail.com>
2025-03-05 07:37:42 +0000

    fisrt

--- services/professeur/src/main/java/com/isi/institution/professeur/ProfesseurRepository.java
+++
@@ -1,6 +0,0 @@
-package com.isi.institution.professeur;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesseurRepository extends JpaRepository<Professeur,Long> {
-}
